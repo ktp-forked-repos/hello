ce740aa40c8277fe894834444f8cc56885741678
Chen Bin <chenbin.sh@gmail.com>
2014-07-24 13:06:28 +1000

    hello world

--- hello.js
+++ hello.js
@@ -1,39 +1,80 @@
-$scope.$watch('tag', function(tag) {
-  $scope.validated = _isTagValid(tag);
-}, true);
+/*global $, app, ko, Raphael, require, angular, setInterval, clearInterval, jQuery, define, KeyEvent, setTimeout, clearTimeout, AccessifyHTML5, log */
+$(document).ready(function (){
+  /* alert("jquery is working"); */
+  function TrelloClipboard() {
+    var me = this;
 
-if (true) {
-  console.log('hello world');
-}
+    var utils = {
+      nodeName: function (node, name) {
+        return node.nodeName.toLowerCase() === name? false : true;
+      }
+    }
+    var textareaId = 'simulate-trello-clipboard',
+    containerId = textareaId + '-container', container, textarea;
 
-function object1()
-{
-}
+    var createTextarea = function () {
+      container = document.querySelector('#' + containerId);
+      if (!container) {
+        container = document.createElement('div');
+        container.id = containerId;
+        container.setAttribute('style', [, 'position: fixed;', 'left: 0px;', 'top: 0px;', 'width: 0px;', 'height: 0px;', 'z-index: 100;', 'opacity: 0;'].join(''));
+        document.body.appendChild(container);
+      }
+      container.style.display = 'block';
+      textarea = document.createElement('textarea');
+      textarea.setAttribute('style', [, 'width: 1px;', 'height: 1px;', 'padding: 0px;'].join(''));
+      textarea.id = textareaId;
+      container.innerHTML = '';
+      container.appendChild(textarea);
 
+      textarea.appendChild(document.createTextNode(me.value));
+      textarea.focus();
+      textarea.select();
+    }
 
-if( document.URL.indexOf('/customercentre/myaccount/myprofile/')!==-1 || document.URL.indexOf('personal/my-account/my-profile')!==-1 ){ $('#editToEmail').click(); } else {window.location.href = '/customercentre/myaccount/myprofile';} return false;
- (function(){
-     //set up css style
-     (function(){
-         var style=''
-                 + 'div {display:block; }\n'
-         ;
-         var styleElement = document.createElement("style");
-         styleElement.type = "text/css";
-         if (styleElement.styleSheet) {
-             styleElement.styleSheet.cssText = style;
-         } else {
-             styleElement.appendChild(document.createTextNode(style));
-         }
-         document.getElementsByTagName("head")[0].appendChild(styleElement);
-     })();
+    var keyDonwMonitor = function (e) {
+      var code = e.keyCode || e.which;
+      if (!(e.ctrlKey || e.metaKey)) {
+        return;
+      }
+      var target = e.target;
+      if (utils.nodeName(target, 'textarea') || utils.nodeName(target, 'input')) {
+        return;
+      }
+      if (window.getSelection && window.getSelection() && window.getSelection().toString()) {
+        return;
+      }
+      if (document.selection && document.selection.createRange().text) {
+        return;
+      }
+      setTimeout(createTextarea, 0);
+    }
 
-     //jquery plugin code placed here
-     (function($) {
-     })(jQuery);
-     //place your code below this line
-     console.log('hello world');
+    var keyUpMonitor = function (e) {
+      var code = e.keyCode || e.which;
+      if (e.target.id !== textareaId) {
+        return;
+      }
+      container.style.display = 'none';
+    }
 
- })();
+    document.addEventListener('keydown', keyDonwMonitor);
+    document.addEventListener('keyup', keyUpMonitor);
+  }
 
-console.log("<html><body><hello></hello><div>hello world</div></body></html>");
+  TrelloClipboard.prototype.setValue = function (value) {
+    this.value = value;
+  }
+
+  var clip = new TrelloClipboard();
+
+  function clipboard(el) {
+    // deselect all
+    var selected = document.getElementsByClassName("selected");
+    for (var i = 0; i < selected.length; i++) {
+      selected[i].className = '';
+    }
+    el.className = 'selected';
+    clip.setValue(el.innerText);
+  }
+});
