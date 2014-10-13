7d293973fbf3bb898b09d8bd56caeebb5e2c0558
Chen Bin <chenbin.sh@gmail.com>
2014-10-13 14:44:52 +1100

    coq sample

---
+++ hello.v
@@ -0,0 +1,9 @@
+Require Import CoqIO.IO.
+Require Import ExtLib.Programming.Show.
+
+Import ShowNotation.
+Local Open Scope show_scope.
+
+Definition main : IO unit :=
+  runShow (M := ShowScheme_Std StdOut)
+          (show_exact "Hello world!" << Char.chr_newline).
